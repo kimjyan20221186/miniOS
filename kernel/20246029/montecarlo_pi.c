2863a7e4323196cc5ca7ef6d2baea023a153935c
kimjyan20221186 <a6230835@cau.ac.kr>
2024-04-19 02:12:20 +0900

    .

--- kernel/20246029/montecarlo_pi.c
+++ kernel/20246029/montecarlo_pi.c
@@ -14,8 +14,7 @@ void* gen_points(void* args) {
     long long num_points = *((long long*)args);
     double x, y;
     long long i, cc_points_local = 0;
-    unsigned int seed = time(NULL) ^ pthread_self();
-
+    unsigned int seed = time(NULL) ^ (unsigned int)pthread_self();
     for (i = 0; i < num_points; i++) {
         x = (double)rand_r(&seed) / RAND_MAX * 2 - 1;
         y = (double)rand_r(&seed) / RAND_MAX * 2 - 1;
@@ -23,11 +22,10 @@ void* gen_points(void* args) {
             cc_points_local++;
         }
     }
-    // 전역 변수 접근 시 뮤텍스로 보호
     pthread_mutex_lock(&mutex);
     circle_points += cc_points_local;
     pthread_mutex_unlock(&mutex);
-    pthread_exit(NULL);
+    return NULL;
 }
 
 void pi(int argc, char* argv[]) {
@@ -35,18 +33,16 @@ void pi(int argc, char* argv[]) {
     long long points_per_thread[NUM_THREADS];
     long long points = NUM_POINTS / NUM_THREADS;
     long long remainder = NUM_POINTS % NUM_THREADS;
-
     // 스레드 생성
     for (int i = 0; i < NUM_THREADS; i++) {
         points_per_thread[i] = points + (i < remainder ? 1 : 0);
         pthread_create(&threads[i], NULL, gen_points, (void *)&points_per_thread[i]);
     }
     // 스레드 종료 대기
-    for (i = 0; i < NUM_THREADS; i++) {
+    for (int i = 0; i < NUM_THREADS; i++) {
         pthread_join(threads[i], NULL);
     }
     // π 추정
     double pi_estimate = 4.0 * circle_points / NUM_POINTS;
     printf("Estimated π = %f\n", pi_estimate);
-}
 }
